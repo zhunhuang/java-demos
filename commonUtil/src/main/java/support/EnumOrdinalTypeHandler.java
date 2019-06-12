bafcf1ea94a81d38d1858e60e97f6ccf006fd426
zhun.huang <zhun.huang@qunar.com>
2019-06-12 15:31:43 +0800

    add

--- commonUtil/src/main/java/support/EnumOrdinalTypeHandler.java
+++ commonUtil/src/main/java/support/EnumOrdinalTypeHandler.java
@@ -11,6 +11,7 @@ import java.sql.SQLException;
 /**
  * mybatis 的 number 和枚举互转 handler
  * @param <E> 要转换的枚举
+ * 这个的实现和{@link CodeEnumTypeHandler} 不一样的地方在于, 使用该typeHandler的枚举必须实现{@link IntegerEnumConvertible} 接口.
  */
 public class EnumOrdinalTypeHandler<E extends IntegerEnumConvertible> extends BaseTypeHandler<E> {
 
@@ -29,12 +30,18 @@ public class EnumOrdinalTypeHandler<E extends IntegerEnumConvertible> extends Ba
 		}
 	}
 
+	/**
+	 * 将 Enum 转换为 integer 传给底层.
+	 */
 	@Override
 	public void setNonNullParameter(PreparedStatement ps, int i, E parameter,
 	                                JdbcType jdbcType) throws SQLException {
 		ps.setObject(i, parameter.getValue());
 	}
 
+	/**
+	 * 将 integer 转换为 Enum 传给上层
+	 */
 	@Override
 	public E getNullableResult(ResultSet rs, String columnName)
 			throws SQLException {
@@ -47,6 +54,9 @@ public class EnumOrdinalTypeHandler<E extends IntegerEnumConvertible> extends Ba
 		}
 	}
 
+	/**
+	 * 将 integer 转换为 Enum 传给上层
+	 */
 	@Override
 	public E getNullableResult(ResultSet rs, int columnIndex)
 			throws SQLException {
@@ -58,6 +68,9 @@ public class EnumOrdinalTypeHandler<E extends IntegerEnumConvertible> extends Ba
 		}
 	}
 
+	/**
+	 * 将 integer 转换为 Enum 传给上层
+	 */
 	@Override
 	public E getNullableResult(CallableStatement cs, int columnIndex)
 			throws SQLException {
@@ -69,7 +82,7 @@ public class EnumOrdinalTypeHandler<E extends IntegerEnumConvertible> extends Ba
 		}
 	}
 
-	public static <E extends IntegerEnumConvertible> E fromCode(Object code, Class<E> enumType) {
+	private static <E extends IntegerEnumConvertible> E fromCode(Object code, Class<E> enumType) {
 		for (E anEnum : enumType.getEnumConstants()) {
 			if (anEnum.getValue().equals(code)) {
 				return anEnum;
